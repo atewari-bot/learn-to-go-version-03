95b792fa6e7a3d2106510501aadbc10be77f3e36
AJ <mail.ajaytewari@gmail.com>
2024-10-14 18:58:19 -0700

    third version

---
+++ 045-mod-code-depend-01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	// "github.com/GoesToEleven/puppy"
+	"github.com/atewari-bot/puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBarks())
+}
